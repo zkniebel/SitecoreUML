f1e6d2ff9beeca0643016bb1da435bc55580918d
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2018-08-26 11:43:45 -0400

    Improvements for new SitecoreDXG service including incorporation of new documentation configuration settings item in Sitecore

---
+++ Sitecore_Module/src/Feature/SitecoreUML/code/Models/JsonSitecoreItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZacharyKniebel.Feature.SitecoreUML.Models
+{
+    [Serializable]
+    public class JsonSitecoreItem : IJsonSitecoreItem
+    {
+        public string ReferenceID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        [System.ComponentModel.DefaultValue(false)]
+        [Newtonsoft.Json.JsonProperty(NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore, DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Populate)]
+        public bool IsTemplate { get; private set; }
+
+        public static implicit operator JsonSitecoreItem(Sitecore.Data.Items.Item item)
+        {
+            return item != null
+                ? new JsonSitecoreItem()
+                    {
+                        ReferenceID = item.ID.ToString(),
+                        Name = item.Name,
+                        Path = item.Paths.Path,
+                        IsTemplate = item.TemplateID == Sitecore.TemplateIDs.Template
+                    }
+                : null;
+        }
+    }
+}
